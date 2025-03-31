ddb8d3885d209efd7c4dd1c171e77481f18c77fd
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-03-31 11:43:34 +0200

    Boom

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 
 
 
-let addNumber;
+/*let addNumber;
 let temporizador;
 let cuentaAtras = 5;
 
@@ -37,3 +37,58 @@ function startCuentaAtras() {
     countDown.innerText = cuentaAtras
 }
 
+*/
+let time = 5
+let userNumber = 0
+
+
+const randomNumber = () => Math.floor(Math.random () * 3) + 1;
+//numero aleatorio entre dos numeros. 
+
+const userInput = document.getElementById("userInput");
+const result    = document.getElementById("result");
+
+userInput.addEventListener("change", () => {
+  userNumber = userInput.value
+
+})
+
+function startGame () {
+  //meter la promesa
+  countDown()
+  const mysteryNumber = new Promise (resolve => {
+    setTimeout(() =>{
+      resolve(randomNumber())
+    }, 6000)
+    return mysteryNumber
+  })
+}
+
+
+function countDown() {
+  setInterval (() =>{
+    if (time >= 0) {
+      countDown.innerText = `Va a explotar en ${time} seg`
+      time--
+    }
+  }, 1000)
+  
+}
+//hacer cuenta atras. Primero pensar que hay que usar el setInterval.
+//luego usar un condicional para que siempre que sea mayor que 0 le reste 1 segundo.
+
+startGame().then ( number => {
+  let mensaje = ""
+  if ( userNumbernumber === number){
+    result.innerHTML = `<p> El numero del usuario es ${userNumber} y el de la maquina es ${number}.Por tanto has salvado el mundo</p>`
+  }else{
+    mensaje = `<p> El numero del usuario es ${userNumber}y el de la maquina es ${number}. Por tanto el mundo ha sido destruido</p>`
+  }
+  result.innerHTML = mensaje
+}) 
+  
+
+restart.addEventListener ("click", function (){
+  location.reload()
+})
+  
